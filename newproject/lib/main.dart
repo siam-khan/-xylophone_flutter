a8effbe4279bc4519bd5b7a11dff76ba8d2bf383
siam-khan <sshinchan97@gmail.com>
2022-03-15 20:29:11 +0600

    comments

--- newproject/lib/main.dart
+++ newproject/lib/main.dart
@@ -1,24 +1,77 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main () {
-  runApp(const XylophineApp());
+  runApp( XylophineApp());
 }
 class XylophineApp extends StatelessWidget {
-  const XylophineApp({ Key? key }) : super(key: key);
+ 
+void playSound(int soundNumber){
+  final player= AudioCache();
+ player.play("note$soundNumber.wav") ;
+}
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(child: Center(
-          child:FlatButton(
-          onPressed: (){
-            final palyer=AudioPlayer();
-            palyer.play("note1.wav");
-          },
-          child: const Text("click mee",),
-        ),
+        body: SafeArea(child: Column(
+          children: [
+            FlatButton(
+              color: Colors.red,
+            onPressed: (){
+              playSound(1);
+            }, child: Text(""),
+            
+            ),
+            FlatButton(
+              color: Colors.black,
+            onPressed: (){
+               playSound(2);
+               
+            },
+            child: Text(""),
+            
+            ),
+            FlatButton(
+              color: Colors.blue,
+            onPressed: (){
+             playSound(3);
+            },
+            child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.orange,
+            onPressed: (){
+             playSound(4);
+            },
+            child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.yellow,
+            onPressed: (){
+               playSound(5);
+            },
+            child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.teal,
+            onPressed: (){
+             playSound(6);
+            },
+            child: Text(""),
+            ),
+            FlatButton(
+              color: Colors.teal,
+            onPressed: (){
+             playSound(7);
+            },
+            child: Text(""),
+            ),
+          ],
         ),
         ),
         ),
--- newproject/test/widget_test.dart
+++ newproject/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:newproject/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const XylophineApp());
+    await tester.pumpWidget( XylophineApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
